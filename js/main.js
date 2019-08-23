14f74c487daa83c852062003d1bbc365ca77db29
Vlad Prodan <vladprodan6@gmail.com>
2019-08-24 01:59:19 +0300

    Responsive, made mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,64 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-   items: 1,
-   loop: true,
-   dots: true
+    items: 2,
+    loop: true,
+    dots: true,
+    margin: 10,
+    pagination: true,
+    smartSpeed: 1000, 
+
+    responsive:{
+      0:{
+          items:1
+          // nav:true
+      },
+      600:{
+          items:1,
+          nav:false
+      },
+      1000:{
+          items:2,
+          // nav:true,
+          loop:false
+      }
+  }
+
+  });
+
+  
+
+  var owl = $('.owl-carousel');
+  owl.owlCarousel();
+  $('#next').click(function () {
+    owl.trigger('next.owl.carousel');
+  })
+ 
+  $('#prev').click(function () {
+  
+    owl.trigger('prev.owl.carousel', [300]);
+  })
+
+  
+
+
+});
+
+
+$(document).ready(function () { 
+  $('.menu-btn').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('menu-btn_active');
+    $('.nav').toggleClass('nav_active');
   });
 });
+
+
+
+
+
+
+
+
+
+
+
